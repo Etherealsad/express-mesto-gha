d4a8f1d131eca7e3a47d3bdcd14ca794697355f2
Mihail Kuksenok <etherealsad@gmail.com>
2022-10-21 14:33:40 +0700

    PR13 card2

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const {
-  getCards, createCard, deleteCardByID, likeCard, dislikeCard,
+  getCards, createCard, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
 router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCardByID);
+router.delete('/cards/:cardId', deleteCardById);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
 
